7a4e235c6a459f7a85adc257280af95d9dafcbb4
201710787 <egnachio@naver.com>
2023-02-07 00:30:30 +0900

    INIT: serverside ts init

---
+++ server/app.ts
@@ -0,0 +1,43 @@
+import { NextFunction, Request, Response } from "express";
+
+require('dotenv').config
+
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(process.env.BUILD_PATH, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(process.env.BUILD_PATH, 'public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ server/routes/index.ts
@@ -0,0 +1,11 @@
+import { NextFunction, Request, Response } from "express";
+
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
