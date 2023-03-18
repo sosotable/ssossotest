52012aed0c62a73732b7ba55eec5d198b93f5b3e
Ujin28 <melon4092@naver.com>
2023-03-18 20:14:08 +0900

    tastes, average 테스트창 사진 추가, 친구에게 공유하기 결과값 오류 수정

--- ssossotest/src/pages/contents/TastesPage.vue
+++ ssossotest/src/pages/contents/TastesPage.vue
@@ -1,22 +1,19 @@
 <template>
   <q-page class="row items-center justify-evenly">
     <div v-if="this.title" class="row items-start q-gutter-md">
-
-
       <div v-if="friendResult">
         <q-card class="my-card" flat bordered>
-          <q-img src="src/assets/images/tastes/title.jpg" />
+          <q-img src="'src/assets/images/tastes/title.jpg'" />
           <q-card-section>
             <div class="text-overline text-orange-9"></div>
             <div class="text-h5 q-mt-sm q-mb-xs">친구 음식 취향 맞추기</div>
             <div class="text-caption text-grey">
-              친구들의 음식 취향을 맞춰보세요!
+              친구의 음식 취향을 맞춰보세요!
             </div>
           </q-card-section>
           <q-btn class = "float-right" flat color="dark" label="시작하기" @click="start"/>
         </q-card>
       </div>
-
 
       <div v-else>
       <q-card class="my-card" flat bordered>
@@ -31,18 +28,8 @@
         <q-btn class = "float-right" flat color="dark" label="시작하기" @click="start"/>
       </q-card>
       </div>
-
     </div>
-    <!--<div v-if="friendResult===true">
-      <q-btn
-        class="text-black"
-        style="width: 100%"
-        label="친구의 결과는 뭐였을까요?"
-        icon="face"
-        color="white"
-        @click="open('bottom')"
-      />
-    </div>-->
+
     <div v-if="this.question && this.questionId < this.tasteModel.length">
       <div class="q-pa-md row justify-center">
         <div style="width: 100%; max-width: 400px">
@@ -51,21 +38,22 @@
               class="card-header"
               style="background-color: transparent !important"
             >
-              <h6 class="card text-center">
+              <h6 class="card text-center" style="margin-top: 10px">
                 {{ this.tasteModel[questionId].question[0] }}
+                <q-img
+                  :src="`/images/tastes/${questionId}.png`"
+                  style="width: 100%; height: 13pc ;margin-top: 10px; margin-left: 0; margin-right: 0px"
+                />
               </h6>
             </div>
-          </div>
-          <Transition
-            class="absolute-bottom flex column"
-            style="padding: 0 10px 200px 0px"
-          >
+            <transition
+              style="padding: 0 10px 0 0"
+            >
             <div v-if="!selectedFlag">
               <template
                 v-for="index in tasteModel[questionId].answer.length"
                 :key="index"
               >
-                <div class="q-btn-group--vertical">
                   <q-btn
                     color="white"
                     text-color="black"
@@ -74,12 +62,12 @@
                     :label="tasteModel[questionId].answer[index - 1].answer"
                   >
                   </q-btn>
-                </div>
               </template>
             </div>
-          </Transition>
+            </transition>
         </div>
       </div>
+    </div>
     </div>
   </q-page>
 </template>
@@ -129,15 +117,11 @@ export default defineComponent({
     },
     // MARK: 문제 버튼 선택 시
     select: function (selected: number) {
-      //console.log(friend)
       this.selectedFlag = true
-      console.log(selected)
       this.selectedAnswer = tasteModel[this.questionId].answer[selected].answer
       console.log(tasteModel[this.questionId].answer[selected].answer)
       console.log(this.selectedAnswer)
       this.tasteModel[this.questionId].result = selected;
-      //this.tasteModel[this.questionId].answer = this.selectedAnswer
-      //console.log(this.tasteModel[this.questionId].answer)
       setTimeout(() => {
         this.questionId += 1;
         this.selectedFlag = false;
@@ -145,7 +129,6 @@ export default defineComponent({
         if (this.questionId == this.tasteModel.length) {
           let result: any = []
           for(let i = 0; i < this.tasteModel.length; i++) {
-            //result.push(this.tasteModel[i].answer)
             result.push(tasteModel[i].answer[this.tasteModel[i].result].answer)
           }
           console.log(result)
@@ -154,10 +137,6 @@ export default defineComponent({
             console.log("메인")
 
             // MARK: 결과 페이지로 라우팅, 결과는 쿼리스트링을 통해 전달
-            /**this.$router.push({
-              path: '/result/tastes',
-              query: {result: JSON.stringify(result)}
-            })**/
               axios.post('/result/tastes', result);
               this.$router.push({
                 path: '/result/tastes',
@@ -172,10 +151,6 @@ export default defineComponent({
             console.log(friend)
             console.log(friend[0])
             console.log(friend[1])
-            /*let newStr: any = []
-            newStr.push(friend.replace('[', ''))
-            let nowStr: any = []
-            nowStr.push(newStr.replace(']', ''))*/
 
             let friend__result = (friend[0].split(','))
 
