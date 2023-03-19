111ed308c07a31cb8fafe6d31467dbcb3f2f4937
hyunjo01 <hyunjo10217@gmail.com>
2023-03-19 14:37:54 +0900

    REFACTOR: average result page

--- ssossotest/src/pages/results/AverageResultPage.vue
+++ ssossotest/src/pages/results/AverageResultPage.vue
@@ -3,7 +3,7 @@
     <div class="row items-start">
       <q-card class="my-card" flat bordered>
         <q-img :src="`src/assets/images/average/${this.image}.jpeg`" />
-        <q-card-section class="col-12">
+        <q-card-section style="width: 300px">
           <div class="text-overline text-orange-9">{{this.userName}}의 결과!</div>
           <div class="q-pa-none col">
             <q-list
@@ -12,11 +12,11 @@
               v-for="(item, i) in this.averageModel"
               :key="i"
             >
-              <q-item-section>{{ item.question }}</q-item-section>
+              <q-item-section style="font-weight: bold">{{ item.question }}</q-item-section>
 
               <div v-if="item.type === 'button' && item.answer[resultList[i]]">
-                <q-item-section>{{ item.answer[resultList[i]].answer }}</q-item-section>
-                <q-item-section>평균</q-item-section>
+                <q-item-section>선택한 값 => {{ item.answer[resultList[i]].answer }}</q-item-section>
+                <q-item-section style="font-weight: bold">평균</q-item-section>
                 <div v-if="i === 1 || i === 2">
                   <q-item-section>{{ averageList[i] }}단계</q-item-section>
                 </div>
@@ -28,9 +28,11 @@
               </div>
 
               <div v-else>
-                <q-item-section>{{ resultList[i] + item.answer[0].unit }}</q-item-section>
-                <q-item-section> 평균:{{ averageList[i] + item.answer[0].unit }}</q-item-section>
-              </div>            </q-list>
+                <q-item-section>선택한 값 => {{ resultList[i] + item.answer[0].unit }}</q-item-section>
+                <q-item-section style="font-weight: bold">평균</q-item-section>
+                <q-item-section>{{ averageList[i] + item.answer[0].unit }}</q-item-section>
+              </div>
+            </q-list>
           </div>
         </q-card-section>
 
